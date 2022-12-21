5a33568dba9e28bc6a125306d086f48d4dbb9214
afondiel <studenetexchange@gmail.com>
2022-12-21 23:12:54 +0100

    del tools repo

--- tools/scripts/run_lanes.bat
+++
@@ -1,3 +0,0 @@
-# -*- coding: cp1252 -*-
-rem run lanes.py file
-python lanes.py
--- tools/scripts/setup.sh
+++
@@ -1,8 +0,0 @@
-# librarie requirements on linux 
-
-sudo apt-get install python3-pip
-sudo pip3 install open-cv-contrib-python
-sudo apt-get update
-sudo apt install python3-opencv
-sudo pip3 install numpy
-sudo pip3 install matplotlib
